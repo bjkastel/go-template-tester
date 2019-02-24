9b570355f016dc3b78b45ab36d3a06f8f42cac7f
Bjoern Kasteleiner <bjoern@kasteleiner.net>
2019-02-24 15:20:00 +0100

    Added input to output line und improved robustness and output layout.

--- main.go
+++ main.go
@@ -1,24 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/Masterminds/sprig"
-	"html/template"
 	"os"
+	"strconv"
+	"text/template"
 )
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("%s [TEMPLATE] [INPUT]...", os.Args[0])
+		os.Exit(1)
+	}
+
 	templateText := os.Args[1]
 	texts := os.Args[2:]
 
-	t, err := template.New("template").Funcs(sprig.FuncMap()).Parse(templateText + "\n")
+	maxLength := 0
+	for _, text := range texts {
+		if len(text) > maxLength {
+			maxLength = len(text)
+		}
+	}
+
+	t, err := template.New("template").Funcs(sprig.TxtFuncMap()).Parse(templateText)
 	if err != nil {
 		panic(err)
 	}
 
 	for _, text := range texts {
-		err = t.Execute(os.Stdout, text)
+		var buffer bytes.Buffer
+		err = t.Execute(&buffer, text)
 		if err != nil {
 			panic(err)
 		}
+		fmt.Printf("%-"+strconv.Itoa(maxLength)+"s: %s\n", text, buffer.String())
 	}
 }
